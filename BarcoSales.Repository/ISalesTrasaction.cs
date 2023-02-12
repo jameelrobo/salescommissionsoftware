4a73a68453ae2fb6b074769fa939b75492002d02
jameelrobo <jameelroboit@gmail.com>
2023-02-12 18:01:30 +0530

    added space after comma only for formated

--- BarcoSales.Repository/ISalesTrasaction.cs
+++ BarcoSales.Repository/ISalesTrasaction.cs
@@ -9,13 +9,13 @@ namespace BarcoSales.Repository
     public interface ISalesTrasaction
     {
         string IGetSalesTrasaction(string connString);
-        string IGetTransactionCustomers(string connString); 
-        
+        string IGetTransactionCustomers(string connString);
 
-        int IDeActiveTransaction(string connString,Int64 TId);
-        int IActiveTransaction(string connString,Int64 TId); 
-        string ISearchTransaction(TransactionSearchRequest transactionSearchRequest, string connString=null);
-      string IManageTransaction(TransactionSearchRequest transactionSearchRequest, string connString=null);
+
+        int IDeActiveTransaction(string connString, Int64 TId);
+        int IActiveTransaction(string connString, Int64 TId);
+        string ISearchTransaction(TransactionSearchRequest transactionSearchRequest, string connString = null);
+        string IManageTransaction(TransactionSearchRequest transactionSearchRequest, string connString = null);
         Salestrasaction IGetSalesTrasactionById(int id);
         Salestrasaction IAddSalesTrasaction(Salestrasaction salesTrasaction);
         Salestrasaction IUpdateSalesTrasaction(Salestrasaction salesTrasaction);
